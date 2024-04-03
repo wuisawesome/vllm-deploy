8d1a27de34b116a7752be7f2ca9467662de5f094
Alex <alexanderwu@berkeley.edu>
2024-04-03 18:01:11 -0400

    better worker startup

--- src/python/vllm_deploy/worker.py
+++ src/python/vllm_deploy/worker.py
@@ -4,6 +4,7 @@ from vllm_deploy.generated import state_pb2
 from vllm_deploy.generated import mailbox_pb2
 from vllm_deploy.logging import init_logging
 from vllm_deploy.serialization import dumps, loads
+from vllm_deploy.importer import import_from_string
 
 import asyncio
 from dataclasses import dataclass
@@ -50,80 +51,37 @@ class ScopeManager:
 
     async def handle_new_event(self, event_proto : mailbox_pb2.NewAsgiEvent):
         scope_id = event_proto.scope_id
+        event = loads(event_proto.payload)
+
         scope = None
         # This locking might not be necessary?
         async with self.scopes_lock:
-            scope = self.scopes[scope_id]
-        assert scope is not None
+            scope = self.scopes.get(scope_id)
 
-        event = loads(event_proto.payload)
-        await scope.received_events.put(event)
-
-
-class AsgiRunner:
-    def __init__(self, app):
-        self.app = app
-        self.scope_manager = ScopeManager()
-        self.grpc_client = WorkerGrpcClient(self.handle_new_scope, self.handle_new_event)
-
-    async def start(self):
-        logger.info(f"Spawning event processing loop")
-        asyncio.create_task(self.grpc_client.start_request_processing())
-        logger.info(f"Spawning control plane loop")
-        asyncio.create_task(self.grpc_client.start_control_plane_connection())
-
-    async def stop(self):
-        await self.grpc_client.stop()
-
-    async def wait_until_completion(self):
-        await self.grpc_client.wait_until_completion()
-
-    async def new_scope_task(self, scope : Scope):
-        receive = scope.received_events.get
-
-        async def send(asgi_event):
-            logger.debug(f"Sending asgi event {asgi_event=}")
-            await self.grpc_client.send_mail(
-                mailbox_pb2.MailMessage(
-                    mailbox_id=scope.router_id,
-                    new_event=mailbox_pb2.NewAsgiEvent(
-                        scope_id=scope.scope_id,
-                        payload=dumps(asgi_event)
-                    )
-                )
-            )
-
-        await self.app(scope.data, receive, send)
-        await self.scope_manager.remove_scope(scope.scope_id)
-
-    async def handle_new_scope(self, scope_proto : mailbox_pb2.NewAsgiScope):
-        scope = await self.scope_manager.handle_new_scope(scope_proto)
-        asyncio.create_task(self.new_scope_task(scope))
-
-    async def handle_new_event(self, event_proto : mailbox_pb2.NewAsgiEvent):
-        await self.scope_manager.handle_new_event(event_proto)
+        if scope is not None:
+            await scope.received_events.put(event)
 
 
 class WorkerGrpcClient:
-    def __init__(self, scope_handler, event_handler):
+    def __init__(self, state_server_address, mailbox_server_address, scope_handler, event_handler):
         self.scope_handler = scope_handler
         self.event_handler = event_handler
         self.worker_id = f"worker-{uuid.uuid4()}"
 
         self.should_stop = False
 
-        self.worker_service_host = "localhost"
-        self.worker_service_port = 8080
-        address = f"{self.worker_service_host}:{self.worker_service_port}"
-        logger.info(f"Connecting to state server on {address}")
-        channel = grpc.aio.insecure_channel(address)
+        # self.worker_service_host = "localhost"
+        # self.worker_service_port = 8080
+        # address = f"{self.worker_service_host}:{self.worker_service_port}"
+        logger.info(f"Connecting to state server on {state_server_address}")
+        channel = grpc.aio.insecure_channel(state_server_address)
         self.worker_service_client = WorkerServiceStub(channel)
 
-        self.mailbox_host = "localhost"
-        self.mailbox_port = 8081
-        address = f"{self.mailbox_host}:{self.mailbox_port}"
-        logger.info(f"Connecting to mailbox server on {address}")
-        channel = grpc.aio.insecure_channel(address)
+        # self.mailbox_host = "localhost"
+        # self.mailbox_port = 8081
+        # mailbox_server_address = f"{self.mailbox_host}:{self.mailbox_port}"
+        logger.info(f"Connecting to mailbox server on {mailbox_server_address}")
+        channel = grpc.aio.insecure_channel(mailbox_server_address)
         self.mailbox_client = MailboxServiceStub(channel)
         self.mailbox_send_stream = None
 
@@ -186,6 +144,50 @@ class WorkerGrpcClient:
             await asyncio.sleep(1)
 
 
+class AsgiRunner:
+    def __init__(self, app, state_server_address, mailbox_server_address):
+        self.app = app
+        self.scope_manager = ScopeManager()
+        self.grpc_client = WorkerGrpcClient(state_server_address, mailbox_server_address, self.handle_new_scope, self.handle_new_event)
+
+    async def start(self):
+        logger.info(f"Spawning event processing loop")
+        asyncio.create_task(self.grpc_client.start_request_processing())
+        logger.info(f"Spawning control plane loop")
+        asyncio.create_task(self.grpc_client.start_control_plane_connection())
+
+    async def stop(self):
+        await self.grpc_client.stop()
+
+    async def wait_until_completion(self):
+        await self.grpc_client.wait_until_completion()
+
+    async def new_scope_task(self, scope : Scope):
+        receive = scope.received_events.get
+
+        async def send(asgi_event):
+            logger.debug(f"Sending asgi event {asgi_event=}")
+            await self.grpc_client.send_mail(
+                mailbox_pb2.MailMessage(
+                    mailbox_id=scope.router_id,
+                    new_event=mailbox_pb2.NewAsgiEvent(
+                        scope_id=scope.scope_id,
+                        payload=dumps(asgi_event)
+                    )
+                )
+            )
+
+        await self.app(scope.data, receive, send)
+        await self.scope_manager.remove_scope(scope.scope_id)
+
+    async def handle_new_scope(self, scope_proto : mailbox_pb2.NewAsgiScope):
+        scope = await self.scope_manager.handle_new_scope(scope_proto)
+        asyncio.create_task(self.new_scope_task(scope))
+
+    async def handle_new_event(self, event_proto : mailbox_pb2.NewAsgiEvent):
+        await self.scope_manager.handle_new_event(event_proto)
+
+
 async def app(scope, receive, send):
     request = await receive()
     logger.info(f"WOOOOHOOOOO app received request {request=}")
@@ -203,7 +205,15 @@ async def app(scope, receive, send):
 
 
 async def main():
-    runner = AsgiRunner(app)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("app")
+    parser.add_argument("--state-server-address", default="localhost:8080")
+    parser.add_argument("--mailbox-server-address", default="localhost:8081")
+    args = parser.parse_args()
+
+    app = import_from_string(args.app)
+    runner = AsgiRunner(app, args.state_server_address, args.mailbox_server_address)
     await runner.start()
     await runner.wait_until_completion()
 
