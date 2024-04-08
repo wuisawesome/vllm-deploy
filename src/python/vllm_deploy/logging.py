1c96491d6bd30dcd4112e0bff76d7af7bb2e441a
Alex <alexanderwu@berkeley.edu>
2024-04-08 15:06:36 -0700

    format properly?

--- src/python/vllm_deploy/logging.py
+++ src/python/vllm_deploy/logging.py
@@ -6,6 +6,6 @@ def init_logging():
     logging.getLogger().setLevel(logging.DEBUG)
     handler = logging.StreamHandler(sys.stderr)
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s:%(lineno) - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logging.getLogger().addHandler(handler)
